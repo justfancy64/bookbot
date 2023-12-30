f0cac2326455ce1797a0f0733149760ce3e6be43
root <root@DESKTOP-DPVTR0T>
2023-12-30 09:56:28 +0200

    small bug fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    book_path = "github.com/justfancy64/bookbot/books/frankenstein.txt"
+    book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
